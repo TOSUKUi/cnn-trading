d60d74f1b697753899bd0de19fd10c45278c33b8
TOSUKUi <am.businessmail.exp@gmail.com>
2019-04-29 20:56:10 +0900

    add train procedure for keras

--- src/strategies/training.py
+++ src/strategies/training.py
@@ -9,7 +9,19 @@ from pipeline import  Procedure
 from sklearn.model_selection import train_test_split
 
 
-class TrainProcedure(Procedure):
+
+class TrainProcedureKeras(Procedure):
+
+    def __init__(self, model): self.model = model
+    
+    def run(self, data):
+        return self.train_model(data)
+
+    def train_model(self, data):
+        return self.model.train(*data)
+    
+
+class TrainProcedureChainer(Procedure):
 
     def __init__(self, model):
         self.model = model 
--- src/train.py
+++ src/train.py
@@ -1,8 +1,9 @@
 from pipeline import PipeLine, TrainPipeLine
 from read_data import ReadData
-from preprocessing import PreprocessingProcedure2D
-from strategies.training import TrainProcedure
-from strategies.neural_net import TradingModel1D, TradingModel2D, MLP
+from preprocessing import PreprocessingProcedure2D, PreprocessingProcedure1D
+from strategies.training import TrainProcedureChainer, TrainProcedureKeras
+from strategies.neural_net import *
+
 import sys
 sys.path.append("/Users/TOSUKUi/Documents/workspace/trading-deeplearning")
 
@@ -12,8 +13,8 @@ def main():
     TrainPipeLine(
         'data/bitstampUSD_1-min_data_2012-01-01_to_2019-03-13.csv', 
         ReadData(), 
-        PreprocessingProcedure2D(), 
-        TrainProcedure(MLP(200, 1))
+        PreprocessingProcedure1D(), 
+        TrainProcedureKeras(KerasLinear1D(saved_model_path="models/saved_model.h5"))
     ).execute()
 
 
