d7548aaf31009253577cf8f7adf873b179f61c8f
TOSUKUi <am.businessmail.exp@gmail.com>
2019-04-30 17:49:17 +0900

    update resample

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -70,7 +70,7 @@ class PreprocessingProcedure1D(Procedure):
         df_b_a = df_accept.bfill()
         df_resampled = df_b_a.resample('60min', how=how)
         df_resampled_1min = df_b_a
-        df_resampled_5min = df_b_a.resample('5min').apply( how=how)
+        df_resampled_5min = df_b_a.resample('5min', how=how)
         df_resampled_15min = df_b_a.resample('15min', how=how)
         X = []
         y = []
