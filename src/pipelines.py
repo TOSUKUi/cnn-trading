a01df37bd07b6d115f6c9a75ad277fecde9f73ae
TOSUKUi <am.businessmail.exp@gmail.com>
2019-05-02 00:37:43 +0900

    add pipelines

---
+++ src/pipelines.py
@@ -0,0 +1,41 @@
+from read_data import ReadData
+from pipeline import PipeLine, TrainPipeLine
+from preprocessing import PreprocessingProcedure1D
+from strategies.training import TrainProcedureChainer, TrainProcedureKeras
+from strategies.neural_net import KerasLinear1D
+from strategies.predicating import PredicateProcedureKeras
+
+
+import sys
+sys.path.append("/Users/TOSUKUi/Documents/workspace/trading-deeplearning")
+
+
+def train_pipeline():
+    saved_model_path = "models/saved_model.h5"
+    data_path = 'data/bitstampUSD_1-min_data_2012-01-01_to_2019-03-13.csv',
+    PipeLine(
+        data_path,
+        ReadData(),
+        PreprocessingProcedure1D(),
+        TrainProcedureKeras(
+            KerasLinear1D(
+                saved_model_path=saved_model_path
+            )
+        )
+    ).execute()
+
+
+def backtest_pipeline():
+    data_path = 'data/bitstampUSD_1-min_data_2012-01-01_to_2019-03-13.csv',
+    saved_model_path = "models/saved_model.h5"
+    PipeLine(
+        data_path,
+        ReadData,
+        PreprocessingProcedure1D(),
+        PredicateProcedureKeras(
+            KerasLinear1D(
+                saved_model_path=saved_model_path,
+                model=open(saved_model_path, "rb")
+            )
+        )
+    ).execute()
