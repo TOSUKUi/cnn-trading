21a40a8893c5401ea882f31d198716de9fa70bcb
TOSUKUi <am.businessmail.exp@gmail.com>
2019-05-03 23:57:32 +0900

    ad strategy order pipeline

--- src/pipelines.py
+++ src/pipelines.py
@@ -1,9 +1,9 @@
 from read_data import ReadData
-from pipeline import PipeLine, TrainPipeLine
-from preprocessing import PreprocessingProcedure1D
+from core.pipeline import PipeLine 
+from preprocessing import TrainingPreprocessingProcedure1D, PreprocessingProcedure1D 
 from strategies.training import TrainProcedureChainer, TrainProcedureKeras
 from strategies.neural_net import KerasLinear1D
-from strategies.predicating import PredicateProcedureKeras
+from strategies.trading_strategies import TradingStrategy
 
 
 import sys
@@ -16,7 +16,7 @@ def train_pipeline():
     PipeLine(
         data_path,
         ReadData(),
-        PreprocessingProcedure1D(),
+        TrainingPreprocessingProcedure1D(),
         TrainProcedureKeras(
             KerasLinear1D(
                 saved_model_path=saved_model_path
@@ -25,13 +25,37 @@ def train_pipeline():
     ).execute()
 
 
+def strategy_order_pipeline():
+    """
+    It is kicked by cron or some scheduler, but you can use it by simple while loop.
+    """
+
+    saved_model_path = "models/saved_model.h5"
+
+    # for example
+
+    ohlcv_columns = ["Open", "High", "Low", "Close", "Volume_(Currency)"]
+    data_path = 'data/bitstampUSD_1-min_data_2012-01-01_to_2019-03-13.csv',
+    p = PipeLine(
+        data_path,
+        ReadData(),
+        PreprocessingProcedure1D(ohlcv_columns),
+        KerasLinear1D(
+            saved_model_path=saved_model_path,
+            model=open(saved_model_path, "rb")
+        ),
+        TradingStrategy()
+    )
+    print(p.execute())
+
+
 def backtest_pipeline():
     data_path = 'data/bitstampUSD_1-min_data_2012-01-01_to_2019-03-13.csv',
     saved_model_path = "models/saved_model.h5"
     PipeLine(
         data_path,
         ReadData,
-        PreprocessingProcedure1D(),
+        PreprocessingProcedure1D(ohlcv_columns=["Open", "High", "Low", "Close", "Volume_(Currency)"]),
         PredicateProcedureKeras(
             KerasLinear1D(
                 saved_model_path=saved_model_path,
