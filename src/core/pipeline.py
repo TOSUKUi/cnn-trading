61516077341ed777bca2628eac2e13ff80589b08
TOSUKUi <am.businessmail.exp@gmail.com>
2019-05-03 23:57:18 +0900

    update: move pipeline to core

---
+++ src/core/pipeline.py
@@ -0,0 +1,21 @@
+from functools import reduce
+from abc import ABCMeta, abstractmethod
+from typing import List
+
+
+class Procedure(metaclass=ABCMeta):
+
+    @abstractmethod
+    def run(self, x):
+        pass
+
+
+class PipeLine():
+
+    def execute(self):
+        # TODO modify it to more flexible pipeline
+        return reduce(lambda x, y: y.run(x), self.pipeline)
+
+    def __init__(self, first, *pipeline: List[Procedure]):
+        self.pipeline = first + pipeline 
+
