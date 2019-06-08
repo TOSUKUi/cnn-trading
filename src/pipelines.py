8cf0c7c4d58ab3b2851f21a9389e0e627214034b
TOSUKUi <am.businessmail.exp@gmail.com>
2019-06-09 00:59:40 +0900

    add: binary

--- src/pipelines.py
+++ src/pipelines.py
@@ -1,8 +1,8 @@
 from read_data import ReadData
 from pipeline import PipeLine 
-from preprocessing import PreprocessingProcedure1D, TrainingPreprocessingProcedure1D
+from preprocessing import PreprocessingProcedure1D, TrainingPreprocessingProcedure1D, TrainingPreprocessingProcedure1DBinary
 from strategies.training import TrainProcedureChainer, TrainProcedureKeras
-from strategies.neural_net import KerasLinear1D
+from strategies.neural_net import KerasLinear1D, KerasLinear1DSoftMax
 from strategies.predicate import PredicateProcedureKeras
 
 
@@ -17,6 +17,22 @@ def train_pipeline():
         TrainingPreprocessingProcedure1D(),
         TrainProcedureKeras(
             KerasLinear1D(
+                saved_model_path=saved_model_path,
+            ),
+            use_early_stop=False
+        )
+    ).execute()
+
+
+def train_pipeline_binary():
+    saved_model_path = "../models/saved_model_binary.h5"
+    data_path = '../data/bitstampUSD_1-min_data_2012-01-01_to_2019-03-13.csv',
+    PipeLine(
+        data_path,
+        ReadData(),
+        TrainingPreprocessingProcedure1DBinary(),
+        TrainProcedureKeras(
+            KerasLinear1DSoftMax(
                 saved_model_path=saved_model_path,
             ),
             use_early_stop=False
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -74,5 +74,37 @@ class TrainingPreprocessingProcedure1D(Procedure):
         return np.array(X, dtype=np.float16), np.array(y, dtype=np.float16)
 
 
+class TrainingPreprocessingProcedure1DBinary(Procedure):
+    
+    def run(self, x):
+        ohlc_dict = {
+            'Open': 'first',
+            'High': 'max',
+            'Low': 'min',
+            'Close': 'last',
+            'Volume_(Currency)': 'sum'
+        }
+        return self.preprocessing(x, ohlc_dict)
+
+    @jit
+    def preprocessing(self, df, how):
+        dataset = []
+        df.loc[:, "datetime"] = pd.to_datetime(df['Timestamp'], unit='s')
+        df_d = df.set_index("datetime")
+
+        df_accept = df_d.loc[datetime(2013, 8, 1):]
+        
+        df_b_a = df_accept.bfill()
+        df_resampled_1min = df_b_a[["Open", "High", "Low", "Close", "Volume_(Currency)"]]
+        X = []
+        y = []
+        for n in tqdm(range(60, len(df_resampled_1min) - 60, 60)):
+            x_base = df_resampled_1min.iloc[n-60:n, :]
+            x_base_normalize = normalize(x_base)
+            y.append(1 if df_resampled_1min["Close"].iloc[n+60] - df_resampled_1min["Close"].iloc[n] > 0 else 0)
+            X.append(x_base_normalize.values)
+        return np.array(X, dtype=np.float16), np.array(y, dtype=np.int8) 
+
+
 def normalize(df):
     return ( df - df.mean() ) / df.std()
